4110b58be4a0023ead18b12a1f96880067b94167
Omkar Bhausaheb Jadhav <omkarjadhav4148@gmail.com>
2023-01-04 16:59:00 +0530

    New commit

--- EmployeeManagement/LoginTest.cs
+++ EmployeeManagement/LoginTest.cs
@@ -22,9 +22,40 @@ namespace EmployeeManagement
             // Landing page Url Confirmation 
             Assert.That(pageUrl, Is.EqualTo("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"));
         }
-        [Test]
-        [TestCase("John", "john123", "Invalid credential")]
-        [TestCase("peter", "peter123", "Invalid credential")]
+        public static object[] InvalidLoginData()
+        {
+            string[] dataset1 = new string[3];
+            dataset1[0] = "John";
+            dataset1[1] = "john123";
+            dataset1[2] = "Invalid credentials";
+
+
+            string[] dataset2 = new string[3];
+            dataset2[0] = "peter";
+            dataset2[1] = "peter123";
+            dataset2[2] = "Invalid credentials";
+
+
+            string[] dataset3 = new string[3];
+            dataset3[0] = "saul";
+            dataset3[1] = "saul123";
+            dataset3[2] = "Invalid credentials";
+
+            object[] allDataSet = new object[3];
+            allDataSet[0] = dataset1;
+            allDataSet[1] = dataset2;
+            allDataSet[2] = dataset3;
+            return allDataSet;
+        }
+
+
+
+
+
+
+        [Test,TestCaseSource(nameof(InvalidLoginData))]
+      //  [TestCase("John", "john123", "Invalid credential")]
+       // [TestCase("peter", "peter123", "Invalid credential")]
         public void InvalidLoginTest(string username, string password, string expectedMsg)
         {
             driver.FindElement(By.Name("username")).SendKeys(username);
